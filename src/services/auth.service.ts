87a53a2db14744910c2906f6728bbf271fff3e22
DenisBeauget <azentiaytbpro@gmail.com>
2025-11-29 16:00:06 +0100

    add android client id

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,58 +5,63 @@ import { SessionRepository } from "../repositories/session.repository.js";
 import { UnauthorizedError } from "../utils/errors.js";
 
 export class AuthService {
-    constructor(private googleClient: OAuth2Client,
-        private userRepository: UserRepository,
-        private accountRepository: AccountRepository,
-        private sessionRepository: SessionRepository
-    ) {}
+  constructor(
+    private googleClient: OAuth2Client,
+    private userRepository: UserRepository,
+    private accountRepository: AccountRepository,
+    private sessionRepository: SessionRepository
+  ) {}
 
+  async findSession(token: string) {
+    return await this.sessionRepository.retrieveByToken(token);
+  }
 
-    async findSession(token: string) {
-        return await this.sessionRepository.retrieveByToken(token);
+  async authenticateWithGoogle(
+    idToken: string,
+    ipAddress: string,
+    userAgent: string
+  ) {
+    const ticket = await this.googleClient
+      .verifyIdToken({
+        idToken,
+        audience: [
+          process.env.GOOGLE_CLIENT_ID!,
+          process.env.GOOGLE_ANDROID_CLIENT_ID!,
+        ],
+      })
+      .catch(() => {
+        throw new UnauthorizedError("Invalid token");
+      });
+
+    const payload = ticket.getPayload();
+    if (!payload?.email) {
+      throw new UnauthorizedError("Invalid token");
     }
 
-    async authenticateWithGoogle(
-        idToken: string,
-        ipAddress: string,
-        userAgent: string
-    ) {
-        const ticket = await this.googleClient.verifyIdToken({
-            idToken,
-            audience: process.env.GOOGLE_CLIENT_ID!
-        }).catch(() => {
-            throw new UnauthorizedError('Invalid token')
-        })
+    let user = await this.userRepository.findByEmail(payload.email);
 
-        const payload = ticket.getPayload();
-        if(!payload?.email) {
-            throw new UnauthorizedError('Invalid token')
-        }
+    if (!user) {
+      user = await this.userRepository.create({
+        email: payload.email,
+        name: payload.name!,
+        image: payload.picture || null,
+      });
 
-        let user = await this.userRepository.findByEmail(payload.email);
-
-        if(!user) {
-            user = await this.userRepository.create({
-                email: payload.email,
-                name: payload.name!,
-                image: payload.picture || null
-            });
-
-            await this.accountRepository.create({
-                userId: user.id,
-                accountId: payload.sub,
-                providerId: 'google',
-                accessToken: null,
-                refreshToken: null
-            });
-        }
-
-        const session = await this.sessionRepository.create(
-            user.id,
-            ipAddress,
-            userAgent
-        );
-
-        return { user, session };
+      await this.accountRepository.create({
+        userId: user.id,
+        accountId: payload.sub,
+        providerId: "google",
+        accessToken: null,
+        refreshToken: null,
+      });
     }
+
+    const session = await this.sessionRepository.create(
+      user.id,
+      ipAddress,
+      userAgent
+    );
+
+    return { user, session };
+  }
 }
